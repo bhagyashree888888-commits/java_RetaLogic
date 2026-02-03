13c739051118b725e0ead98ab6c64863a18e01b2
Bhagyashree <bhagyashree.888888@gmail.com>
2026-02-03 08:26:31 +0530

    3-2-26@08.25 @commit

--- src/main/java/org/example/exceptionHandling/throwVSthrows.java
+++ src/main/java/org/example/exceptionHandling/throwVSthrows.java
@@ -11,17 +11,38 @@ package org.example.exceptionHandling;
 //}
 //}
 
+import java.io.IOException;
+
 public class throwVSthrows {
 
     // throw
     static void divide() {
         int a = 10 / 0;   // JVM throws ArithmeticException here
+        System.out.println("before line of unreachable statement code");
         throw new ArithmeticException("/ by zero"); // ❌ never reached
+        //System.out.println("unreachable code "); //compile time error
     }
-
+ static void call(){
+     System.out.println("here is call method");
+ }
     public static void main(String[] args) {
+//        throwVSthrows.call();
         divide();
+        throwVSthrows.call();
         System.out.println("This line will not execute");
+        //checked excp
+//        try {
+//            int a = 10 / 2;   // no checked exception possible
+//        } catch (IOException e) {
+//            System.out.println("Error");
+//        }
+        try {
+            int a = 10 / 2;
+        } catch (ArithmeticException e) {
+            System.out.println("Error");
+        }
+
+
     }
 }
 
